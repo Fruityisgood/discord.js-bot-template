8502ac3aa39501a8a964719dbdc51f5a3209a1f6
Fruity <penguinttskm@gmail.com>
2021-10-01 06:18:21 -0400

    added some stuff

---
+++ install.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+ECHO Welcome to installing! (Press a key to start) Once installing is done please proceed to setup.bat
+
+npm install
---
+++ setup.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+ECHO All the installing is done! Now comes the fun bit! Let's customize your bot! Press a key to start!
+
+PAUSE
+
+ECHO Put your bot token at the bottom of main.js and customize the bot however you want (this is a template so do anything with it) When you are done press any button.
+
+PAUSE
+
+ECHO Your bot has been successfully been  setup. Run start.bat to run your bot. Credit  me(Fruity#9199) if you can. Press a button to close this console.
+
+PAUSE
