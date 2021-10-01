2599f93e69acfa9d663699de68127af33e6a2398
Fruity <penguinttskm@gmail.com>
2021-10-01 11:29:29 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'help',
     description: "Get help!",
     execute(message, args){
-        message.channel.send('Make more files in commands to add cool commands. Change this command to something that fits you by going to help.js');
+        message.channel.send('Run >tutorial for a tutorial on how to use this.');
     }
 }
