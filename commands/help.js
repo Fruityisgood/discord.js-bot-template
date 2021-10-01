f9748cd8f60ef199d1b76ba5e6e41b17c8e63b4f
Fruity <penguinttskm@gmail.com>
2021-10-01 11:21:29 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 module.exports = {
-    name: 'ping',
+    name: 'help',
     description: "Get help!",
     execute(message, args){
-        message.channel.send('pong!');
+        message.channel.send('Make more files in commands to add cool commands. Change this command to something that fits you by going to help.js');
     }
 }
