6ce6e3bf0296cad7af579ef9fb01a65f23b00aed
Fruity <penguinttskm@gmail.com>
2021-10-01 11:27:35 +0100

    update

--- commands/tutorial.js
+++ commands/tutorial.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+    name: 'tutorial',
+    description: "Find a tutorial!",
+    execute(message, args){
+        message.channel.send('Tutorials aren not out yet however make sure to sub to https://www.youtube.com/channel/UCEOtdWqCSPV_2IFUysxwVuQ if you wish to see s tutorial in the future :)');
+    }
+}
